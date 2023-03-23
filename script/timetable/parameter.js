1f33ad6c91a549b78c868a428f542bce32040efe
BackGwa <backgwa@icloud.com>
2023-03-23 20:07:22 +0900

    테스트

--- script/timetable/parameter.js
+++ script/timetable/parameter.js
@@ -7,9 +7,3 @@ function getParameterByName(name) {
     if (!results[2]) return "";
     return decodeURIComponent(results[2].replace(/\+/g, " "));
   }
-
-var _Grade = getParameterByName("grade");
-var _Class = getParameterByName("class");
-
-const TitleMessage = document.getElementById('title');
-TitleMessage.innerHTML = `${_Grade}학년 ${_Class}반`;

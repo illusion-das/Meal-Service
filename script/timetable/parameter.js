b61c6ed4b8bf8658d746a00ba499030b5aded902
BackGwa <backgwa@icloud.com>
2023-03-23 20:12:28 +0900

    fix

--- script/timetable/parameter.js
+++ script/timetable/parameter.js
@@ -9,3 +9,9 @@ var urlParams = location.search.split(/[?&]/).slice(1).map(function(paramPair) {
     return obj;
 
 }, {});
+
+var _Grade = urlParams.grade;
+var _Class = urlParams.sclass;
+
+const TitleMessage = document.getElementById('title');
+TitleMessage.innerHTML = `${_Grade}학년 ${_Class}반`;

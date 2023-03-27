2026c152cc7cd3552ae8cbf34055ba005442701a
BackGwa <backgwa@icloud.com>
2023-03-27 19:32:28 +0900

    날짜에 따른 변화

--- script/timetable/parameter.js
+++ script/timetable/parameter.js
@@ -14,12 +14,26 @@ function getParams(){
 function ax(){
   var pag = getParams();
 
-  console.log(pag);
-  
   var _Grade = pag["grade"];
-  console.log(_Grade);
   var _Class = pag['class'];
-  console.log(_Class);
   const tm = document.getElementById('titlex');
   tm.innerHTML = _Grade + '학년 ' + _Class + '반';
+
+  var datev = new Date();
+
+  datx = getDayOfWeek(`${datev.getFullYear()}-${datev.getMonth() + 1}-${datev.getDate()}`);
+
+  const indatx = document.getElementById('times' + datx);
+  indatx.classList.add('toptime');
+
+}
+
+function getDayOfWeek(dstr){
+
+  const week = ['0', '1', '2', '3', '4', '5', '6'];
+
+  const dayOfWeek = week[new Date(dstr).getDay()];
+
+  return dayOfWeek;
+
 }
